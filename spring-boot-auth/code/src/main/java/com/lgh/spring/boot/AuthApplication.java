bab2b7c959601c7fe8e35a2bc853842d824c379a
liguanghao <chiansoft_lgh@outlook.com>
2017-03-04 20:15:08 +0800

    aa

--- spring-boot-auth/code/src/main/java/com/lgh/spring/boot/AuthApplication.java
+++ spring-boot-auth/code/src/main/java/com/lgh/spring/boot/AuthApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer{
+public class AuthApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 	@Override
 	protected SpringApplicationBuilder configure(
 			SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
+		return application.sources(AuthApplication.class);
 	}
 }
